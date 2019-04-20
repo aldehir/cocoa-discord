404433a42f998e6e76ba8e130405c3cbbde09c66
Alde Rojas <hello@alde.io>
2019-04-20 22:05:45 +0000

    Refactor cocoa

--- src/cocoa.ts
+++ src/cocoa.ts
@@ -1,33 +1,52 @@
 import * as Discord from 'discord.js'
 
 import logger from './logger'
+import Router from './router'
+import { Command } from './parser'
 
 interface CocoaConfig {
-  token: string
+  token: string,
+  prefix?: string
 }
 
 export default class Cocoa {
-  config: CocoaConfig
-  client: Discord.Client
+  readonly token: string
+  readonly prefix: string
+
+  private client: Discord.Client
+  private router: Router
 
   constructor(config: CocoaConfig) {
-    this.config = config
-    this.client = new Discord.Client()
-    this.setup()
-  }
+    this.token = config.token
+    this.prefix = config.prefix || '!'
 
-  private setup() {
-    this.client.on("message", (msg) => {
-      if (msg.content == "ping") {
-        let guildChannel = msg.guild.channels.find((ch) => ch.id === msg.channel.id)
-        logger.info(`Received ping from ${msg.author.username} in ${msg.guild.name}#${guildChannel.name}`)
-        msg.reply("pong")
-      }
-    })
+    this.client = new Discord.Client()
+    this.router = new Router({ prefix: this.prefix })
+
+    this.setupClient()
+    this.setupRouter()
   }
 
   run() {
-    this.client.login(this.config.token)
+    this.client.login(this.token)
     logger.info('Cocoa running')
+  }
+
+  private setupClient() {
+    this.client.on("message", this.processMessage.bind(this))
+  }
+
+  private setupRouter() {
+    this.router.route('help', this.onHelp.bind(this))
+  }
+
+  private processMessage(msg: Discord.Message) {
+    logger.info(`Received message '${msg.content}' from ${msg.author.username}`)
+    this.router.process(msg.content, msg)
+  }
+
+  private onHelp(command: Command, msg: any) {
+    let message = msg as Discord.Message 
+    message.channel.sendCode('text', `usage: ${this.prefix}help`)
   }
 }
